77f2066ccfdaad94976777ab03e99df1ec16b20c
Martin Kimmig <martin.kimmig@students.fhnw.ch>
2017-10-20 09:23:59 +0200

    Luftwiderstand hinzugefügt + update von Geschwindigkeit läuft jetzt besser

--- src/programs/SchussAufBlech.java
+++ src/programs/SchussAufBlech.java
@@ -38,15 +38,15 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 	float znear = -100, zfar = 100;
 
 	JFrame parent = new JFrame();
-	// Circle values 
-	
+	// Circle values
+
 	final double g = 9.81; // Erdbeschleunigung
 	final double m = 1; // Masse
 	double v0x = 8; // Anfangsgeschwindigkeit
 	double v0y = 10; // Anfangsgeschwindigkeit
 	// Zum zurücksetzen
 	double x0 = -8;
-	double y0 = 0;
+	double y0 = -4;
 	// ---------------
 	double x = x0;
 	double y = y0;
@@ -56,25 +56,32 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 	double ay = -g;
 	double dt = 0.01; // Zeitschritt
 	boolean stopped = false;
+	
 
 	// Rectangle
-	
+
 	// for the case when y reaches ybotton, we need to reset these values
 	final double startPositionXForReset = 8;
 	final double startPositionYForReset = 5;
-	
+
 	// we will use these values for calculating
 	double startPositionX = startPositionXForReset;
 	double startPositionY = startPositionYForReset;
-	
+
 	double sizeRectangleLength = 1;
 	double sizeRectangleHeight = 2;
-	
-	// Velocity, with which the rectangle moves to the x Koordinate (decreases y Koordinate)
+
+	// Velocity, with which the rectangle moves to the x Koordinate (decreases y
+	// Koordinate)
 	final double velocity = -9.3;
-	
+
 	// only 20 chances
 	int counter = 0;
+
+	// Luftwiderstand
+	boolean luftwiderstand;
+	double c = 0.025;
+	
 	
 	// --------- Methoden ----------------------------------
 
@@ -96,13 +103,15 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 		f.setVisible(true);
 		f.addKeyListener(this);
 		canvas.addKeyListener(this);
-	}	
-	
+	}
+
 	// Objekte
 
 	public void zeichneRechteck(GL3 gl, float x1, float y1, float width, float height) {
+
 		mygl.rewindBuffer(gl); // Vertex-Buffer zuruecksetzen
-		// 1. Paramter x-Koordinate; 2. Paramter y-Koordinate; 3. Paramter z-Koordinate -> 
+		// 1. Paramter x-Koordinate; 2. Paramter y-Koordinate; 3. Paramter
+		// z-Koordinate ->
 		// hier gleich 0, da nur 2Dimensional
 		mygl.putVertex(x1, y1, 0);
 		mygl.putVertex(x1 + width, y1, 0);
@@ -129,7 +138,7 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 		mygl.copyBuffer(gl);
 		mygl.drawArrays(gl, GL3.GL_TRIANGLE_FAN);
 	}
-	
+
 	// ---------- OpenGL-Events ---------------------------
 
 	@Override
@@ -144,83 +153,140 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 																		// Shader-Programme
 		mygl = new MyGLBase1(gl, programId, maxVerts); // OpenGL
 														// Basis-Funktionen
+		
+		
+		
 		FPSAnimator anim = new FPSAnimator(canvas, 200, true);
+
 		anim.start();
+
 	}
 
 	@Override
 	public void display(GLAutoDrawable drawable) {
-		if (stopped){
-		
-		
+
 		GL3 gl = drawable.getGL().getGL3();
 		gl.glClear(GL3.GL_COLOR_BUFFER_BIT); // Bildschirm loeschen
-		mygl.setColor(0, 1, 0); // Farbe der Vertices
-
-		double alpha = 0;
+		mygl.setColor(0, 1, 0); // Farbe der Vertices		
 
 		M = Mat4.ID;
 		mygl.setM(gl, M);
+		
+		
 		zeichneKreis(gl, 0.2f, (float) x, (float) y, 20);
 
+		zeichneRechteck(gl, (float) startPositionX, (float) startPositionY, (float) sizeRectangleLength,
+				(float) sizeRectangleHeight);
+
 		M = Mat4.translate((float) x, (float) y, 0);
-		alpha = (Math.atan(vy / vx)) * (180 / Math.PI);
-		//M = M.postMultiply(Mat4.rotate((float) alpha, 0, 0, 1));
-		//mygl.setM(gl, M);
+		
+		// M = M.postMultiply(Mat4.rotate((float) alpha, 0, 0, 1));
+		// mygl.setM(gl, M);
 		// zeichneSpeer(gl, 1.2f, 0.04f, 0.2f);
-		
-		
-		
-		// eulerischer Algorythmus 2D
-		
 
-		x = x + vx * dt;
-		y = y + vy * dt;
-		vx = vx + ax * dt;
-		vy = vy + ay * dt;
-		if (y < ybottom || x > xright) {
-			counter++;
-			stopped = false;
-			x = x0;
-			y = y0;
-			vx = v0x;
-			vy = v0y;
-		}
-		
-		zeichneRechteck(gl, (float)startPositionX, (float)startPositionY, (float)sizeRectangleLength, (float)sizeRectangleHeight);
-		
-		// y = y + v*dt;
-		// v = v + -g*dt;
-		startPositionY = startPositionY + velocity * dt;
-		
-		//falls startPosition Y in negativen Bereich kommt:
-		if (startPositionY < ybottom){
-			startPositionY = startPositionYForReset;
+		if (stopped) {
+
+			// eulerischer Algorythmus 2D
+
+			x = x + vx * dt;
+			y = y + vy * dt;
+			vx = vx + ax * dt;
+			vy = vy + ay * dt;
 			
+			
+			if (y < ybottom || x > xright) {
+				counter++;
+				stopped = false;
+				x = x0;
+				y = y0;
+				vx = v0x;
+				vy = v0y;
+				startPositionY = startPositionYForReset;
+			}
+
+			// y = y + v*dt;
+			// v = v + -g*dt;
+			startPositionY = startPositionY + velocity * dt;
+
+			// falls startPosition Y in negativen Bereich kommt:
+			if (startPositionY < ybottom) {
+				startPositionY = startPositionYForReset;
+
+			}
+
+			if (counter > 3) {
+				JDialog.setDefaultLookAndFeelDecorated(true);
+				int response = JOptionPane.showConfirmDialog(null, "3 Versuche gehabt. Neu starten?", "Confirm",
+						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+				if (response == JOptionPane.NO_OPTION) {
+					System.out.println("No button clicked");
+					System.exit(1);
+				} else if (response == JOptionPane.YES_OPTION) {
+					System.out.println("Yes button clicked");
+					counter = 0;
+				} else if (response == JOptionPane.CLOSED_OPTION) {
+					System.exit(1);
+				}
+			}
+
+			// falls Luftwiderstand eingestellt
+
+			if (luftwiderstand == true) {
+
+				// eulerischer Algorythmus 2D
+				
+				
+				x = x +  -c * Math.sqrt(Math.pow(vx, 2)+Math.pow(vy, 2))  * dt;
+				y = y + -c * Math.sqrt(Math.pow(vx, 2)+Math.pow(vy, 2))  * dt;
+				
+				vx = vx + ax * dt;
+				vy = vy + ay * dt;
+				
+				
+				
+				if (y < ybottom || x > xright) {
+					counter++;
+					stopped = false;
+					x = x0;
+					y = y0;
+					vx = v0x;
+					vy = v0y;
+					startPositionY = startPositionYForReset;
+				}
+
+				// y = y + v*dt;
+				// v = v + -g*dt;
+				startPositionY = startPositionY + velocity * dt;
+
+				// falls startPosition Y in negativen Bereich kommt:
+				if (startPositionY < ybottom) {
+					startPositionY = startPositionYForReset;
+
+				}
+
+				if (counter > 3) {
+					JDialog.setDefaultLookAndFeelDecorated(true);
+					int response = JOptionPane.showConfirmDialog(null, "3 Versuche gehabt. Neu starten?", "Confirm",
+							JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+					if (response == JOptionPane.NO_OPTION) {
+						System.out.println("No button clicked");
+						System.exit(1);
+					} else if (response == JOptionPane.YES_OPTION) {
+						System.out.println("Yes button clicked");
+						counter = 0;
+					} else if (response == JOptionPane.CLOSED_OPTION) {
+						System.exit(1);
+					}
+				}
+
+			}
 		}
-		
-		if (counter > 3){
-			   JDialog.setDefaultLookAndFeelDecorated(true);
-			    int response = JOptionPane.showConfirmDialog(null, "3 Versuche gehabt. Neu starten?", "Confirm",
-			        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-			    if (response == JOptionPane.NO_OPTION) {
-			      System.out.println("No button clicked");
-			      System.exit(1);
-			    } else if (response == JOptionPane.YES_OPTION) {
-			      System.out.println("Yes button clicked");
-			      counter = 0;
-			    } else if (response == JOptionPane.CLOSED_OPTION) {
-			      System.exit(1);
-			    }
-		}
-		}
-		
+
 	}
-
-
 
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+
 		GL3 gl = drawable.getGL().getGL3();
 		// Set the viewport to be the entire window
 		gl.glViewport(0, 0, width, height);
@@ -274,11 +340,20 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 		switch (key) {
 		case KeyEvent.VK_UP:
 			v0y += 0.5;
+			v0x += 0.5;
+			
+			vx += 0.5;
+			vy += 0.5;
 			break;
 		case KeyEvent.VK_DOWN:
 			v0y -= 0.5;
+			v0x -= 0.5;
+			
+			vy -= 0.5;
+			vx -= 0.5;			
 			break;
 		}
+
 	}
 
 	@Override
@@ -295,6 +370,11 @@ public class SchussAufBlech implements WindowListener, GLEventListener, KeyListe
 		if (code == 's') {
 			stopped = true;
 		}
+		if (code == 'l') {
+			luftwiderstand = true;
+		}
+
+		
 
 	}
 
